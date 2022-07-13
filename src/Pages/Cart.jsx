f41687e70ffc44e6b6f5069e43db4ea91bb16ac7
Abdi-01 <abdialghi@gmail.com>
2022-07-13 11:46:20 +0700

    cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -33,18 +33,27 @@ const CartPage = (props) => {
         let newData = {
             ...temp[idx]
         }
-        newData.qty+=1
-        // temp.splice(idx, 1, newData); //Cara 1
-        temp[idx]=newData; // Cara 2
 
-        axios.patch(API_URL + `/users/${id}`, {
-            cart: temp
-        }).then((res) => {
-            console.log('reponse', res.data.cart)
-            dispatch(updateCartAction(res.data.cart));
-        }).catch((err) => {
-            console.log(err)
-        })
+        axios.get(API_URL + `/products?id=${idProduct}`)
+            .then((resGet) => {
+                if (newData.qty < resGet.data[0].stock) {
+                    newData.qty += 1
+                    // temp.splice(idx, 1, newData); //Cara 1
+                    temp[idx] = newData; // Cara 2
+
+                    axios.patch(API_URL + `/users/${id}`, {
+                        cart: temp
+                    }).then((res) => {
+                        console.log('reponse', res.data.cart)
+                        dispatch(updateCartAction(res.data.cart));
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+                }
+            }).catch((errGet) => {
+                console.log(errGet);
+            })
+
     }
 
     const onDec = () => {
